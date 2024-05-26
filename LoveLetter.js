c57336d0a79b7c508e9f6c42dc2afb140a9e38e3
krishna050504 <krishna06770@gmail.com>
2024-05-26 09:37:18 +0530

    Add files via upload

---
+++ LoveLetter.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './tu hai.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        
+Happy Anniversary, Mom and Dad!🖤,<br />
+Your love is my greatest inspiration...<br />Thank you for being the best parents and showing me the beauty of true love. :)<br />
+Love always,<br />
+        @Your Little Queen
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
